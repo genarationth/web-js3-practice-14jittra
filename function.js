f73b17363a74b304c9e33d4371d4140d2f69f841
14jittra <125845796+14jittra@users.noreply.github.com>
2023-03-13 16:56:37 +0700

    Add files via upload

---
+++ function.js
@@ -0,0 +1,20 @@
+function add(a,b) {
+    console.log(a + b);
+}
+
+function subtract(a,b) {
+    console.log(a - b);
+}
+
+function multiply(a,b) {
+    console.log(a * b);
+}
+
+function divide(a,b) {
+    console.log(a / b);
+}
+
+add(8,4);          
+subtract(8,4);     
+multiply(8,4);     
+divide(8,4);       
