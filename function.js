ee8036f68837718e79ee25b7046246443a727eb8
14jittra <125845796+14jittra@users.noreply.github.com>
2023-03-13 16:58:56 +0700

    Update function.js

--- function.js
+++ function.js
@@ -18,3 +18,9 @@ add(8,4);
 subtract(8,4);     
 multiply(8,4);     
 divide(8,4);       
+// exercise 2 inform
+function info(firstName, location, hobby) {
+    console.log("Hi, my name is " + firstName + ", I live in " + location + " and enjoy "+ hobby);
+}
+
+info('ji', 'chaing rai', 'eating food');
